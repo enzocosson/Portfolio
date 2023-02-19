12d18923d137de47475dda25552dfcfd89c71d50
enzocosson <enzolemercier@gmail.com>
2023-02-19 03:34:40 +0100

    semantique page photo finit

--- js/scrollMagicGalerie.js
+++ js/scrollMagicGalerie.js
@@ -1,20 +1,9 @@
 export function scrollMagicGalerie() {
   const pathCol1 = {
     curviness: 1.25,
-    values: [{  transform: "translateY(-40vh)",}],
-  };
-  const pathCol2 = {
-    curviness: 1.25,
     values: [{  transform: "translateY(-30vh)",}],
   };
-  const pathCol3 = {
-    curviness: 1.25,
-    values: [{  transform: "translateY(-50vh)",}],
-  };
-  const pathCol4 = {
-    curviness: 1.25,
-    values: [{  transform: "translateY(-20vh)",}],
-  };
+
   const pathMiniatures = {
     curviness: 1.25,
     values: [
@@ -34,68 +23,195 @@ export function scrollMagicGalerie() {
     )
     .add(
       TweenMax.to(".col2", 1, {
-        bezier: pathCol2,
+        bezier: pathCol1,
         ease: Power1.easeInOut,
       }),
       "-=1"
     )
     .add(
       TweenMax.to(".col3", 1, {
-        bezier: pathCol3,
+        bezier: pathCol1,
         ease: Power1.easeInOut,
       }),
       "-=1"
     )
     .add(
       TweenMax.to(".col4", 1, {
-        bezier: pathCol4,
+        bezier: pathCol1,
         ease: Power1.easeInOut,
       }),
       "-=1"
     )
     .add(
-      TweenMax.to(".miniatures", 1, {
+      TweenMax.to(".galerie_first .miniatures", 1, {
         bezier: pathMiniatures,
         ease: Power1.easeInOut,
       }),
       "=-1"
-    );
+    )
+;
   const Controller = new ScrollMagic.Controller();
 
   const scene = new ScrollMagic.Scene({
-    triggerElement: ".galerie_web_page",
-    duration: 3500,
-    triggerHook: 0,
+    triggerElement: ".galerie_first",
+    duration: 4000,
+    triggerHook: 0.5,
   })
     .setTween(galerie)
     .addTo(Controller);
 
   // --------------------------------------------
 
-  const pathSlider = {
+  const pathCol2 = {
     curviness: 1.25,
-    values: [{  transform: "translateX(-200vw)",}],
+    values: [{  transform: "translateY(-50vh)",}],
   };
 
+  const pathMiniatures2 = {
+    curviness: 1.25,
+    values: [
+      {
+        top: "0%",
+      },
+    ],
+  };
+
+  const galerie2 = new TimelineMax()
+    .add(
+      TweenMax.to(".container_img_100", 1, {
+        bezier: pathCol2,
+        ease: Power1.easeInOut,
+      }),
+      "-=1"
+    )
+    .add(
+      TweenMax.to(".container_img_50", 1, {
+        bezier: pathCol2,
+        ease: Power1.easeInOut,
+      }),
+      "-=1"
+    )
+    .add(
+      TweenMax.to(".galerie_large .miniatures", 1, {
+        bezier: pathMiniatures,
+        ease: Power1.easeInOut,
+      }),
+      "=-1"
+    );
+  const Controller3 = new ScrollMagic.Controller();
+
+  const scene2 = new ScrollMagic.Scene({
+    triggerElement: ".galerie_large",
+    duration: 3000,
+    triggerHook: 0.5,
+  })
+    .setTween(galerie2)
+    .addTo(Controller3);
+
+  // --------------------------------------------
+
+  const pathSliderTop = {
+    curviness: 1.25,
+    values: [{  transform: "translateX(-136vw)",}],
+  };
+  const pathSliderBottom = {
+    curviness: 1.25,
+    values: [{  transform: "translateX(136vw)",}],
+  };
   const slider = new TimelineMax()
     .add(
-      TweenMax.to(".slider_galerie", 1, {
-        bezier: pathSlider,
+      TweenMax.to(".slider_galerie_top", 1, {
+        bezier: pathSliderTop,
+        ease: Power1.easeInOut,
+      }),
+      "-=1"
+    ).add(
+      TweenMax.to(".slider_galerie_bottom", 1, {
+        bezier: pathSliderBottom,
         ease: Power1.easeInOut,
       }),
       "-=1"
     );
   const Controller2 = new ScrollMagic.Controller();
 
-  const scene2 = new ScrollMagic.Scene({
+  const scene3 = new ScrollMagic.Scene({
     triggerElement: ".container_slider_galerie",
     duration: 3000,
-    triggerHook: 1.5,
+    triggerHook: 0,
   })
     .setTween(slider)
-    // .setPin(".container_slider_galerie")
+    .setPin(".container_slider_galerie")
     .addTo(Controller2);
 
 
 
+  // --------------------------------------------
+
+  const pathLanscape = {
+    curviness: 1.25,
+    values: [{  width: "80%", height: "80%"}],
+  };
+  const pathMinia = {
+    curviness: 1.25,
+    values: [
+      {
+        top: "0%",
+      },
+    ],
+  };
+  
+  const landscape = new TimelineMax()
+    .add(
+      TweenMax.to(".container_galerie_bottom .container_img", 1, {
+        bezier: pathLanscape,
+        ease: Power1.easeInOut,
+      })
+    ).add(
+      TweenMax.to(".container_galerie_bottom .miniatures", 1, {
+        bezier: pathMinia,
+        ease: Power1.easeInOut,
+      }),
+      "-=1"
+    );
+  const Controller4 = new ScrollMagic.Controller();
+
+  const scene4= new ScrollMagic.Scene({
+    triggerElement: ".container_galerie_bottom",
+    duration: 3000,
+    triggerHook: 0,
+  })
+    .setTween(landscape)
+    .setPin(".container_galerie_bottom")
+    .addTo(Controller4);
+
+
+
+
+  // --------------------------------------------
+
+  const pathSuggestion = {
+    curviness: 1.25,
+    values: [
+      {
+        top: "0%",
+      },
+    ],
+  };
+  
+  const suggestion = new TimelineMax().add(
+      TweenMax.to(".autres_projets .miniatures", 1, {
+        bezier: pathSuggestion,
+        ease: Power1.easeInOut,
+      }),
+      "-=1"
+    );
+  const Controller5= new ScrollMagic.Controller();
+
+  const scene5 = new ScrollMagic.Scene({
+    triggerElement: ".autres_projets",
+    duration: 2000,
+    triggerHook: 0.5,
+  })
+    .setTween(suggestion)
+    .addTo(Controller5);
 }
