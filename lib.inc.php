db36b1bce07055da79f88ceecb6a17d50731fffc
enzocosson <enzolemercier@gmail.com>
2023-03-11 23:06:16 +0100

    securisation formulaire de contact php

--- lib.inc.php
+++ lib.inc.php
@@ -22,33 +22,43 @@ function sendMessage($co, $prenom, $nom, $email, $theme, $message)
     $date = date('Y-m-d'); // format de date: AAAA-MM-JJ
     $heure = date('H:i:s'); // format d'heure: HH:MM:SS
 
+    // Validation des données d'entrée utilisateur
+    if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        echo "<h1>Adresse email invalide</h1>";
+        return;
+    }
+    if (!preg_match("/^[a-zA-Z ]*$/", $prenom)) {
+        echo "<h1>Le prénom doit contenir uniquement des lettres et des espaces</h1>";
+        return;
+    }
+    if (!preg_match("/^[a-zA-Z ]*$/", $nom)) {
+        echo "<h1>Le nom doit contenir uniquement des lettres et des espaces</h1>";
+        return;
+    }
+    if (strlen($message) > 500) {
+        echo "<h1>Le message est trop long</h1>";
+        return;
+    }
+
     $req = 'INSERT INTO portfolio (prenom, nom, email, theme, message, date, heure) 
-    VALUES ("' . $prenom . '","' . $nom . '","' . $email . '","' . $theme . '","' . $message . '","' . $date . '","' . $heure . '")';
+            VALUES (:prenom, :nom, :email, :theme, :message, :date, :heure)';
 
     try {
         $resultat = $co->prepare($req);
+        $resultat->bindParam(':prenom', $prenom, PDO::PARAM_STR);
+        $resultat->bindParam(':nom', $nom, PDO::PARAM_STR);
+        $resultat->bindParam(':email', $email, PDO::PARAM_STR);
+        $resultat->bindParam(':theme', $theme, PDO::PARAM_STR);
+        $resultat->bindParam(':message', $message, PDO::PARAM_STR);
+        $resultat->bindParam(':date', $date, PDO::PARAM_STR);
+        $resultat->bindParam(':heure', $heure, PDO::PARAM_STR);
         $resultat->execute();
     } catch (PDOException $e) {
         echo '<p>Erreur : ' . $e->getMessage() . '</p>';
         die();
     }
-
     if ($resultat !== false) {
-        $_SESSION['prenom_user'] = $prenom;
-        echo $_SESSION['prenom_user'];
-
-        // Envoi du mail
-        $to = "enzolemercier@gmail.com";
-        $subject = "Nouvelle inscription dans la base de données";
-        $body = "Une nouvelle ligne a été insérée dans la base de données.\n\n";
-        $body .= "Prénom : $prenom\n";
-        $body .= "Nom : $nom\n";
-        $body .= "Email : $email\n";
-        $body .= "Thème : $theme\n";
-        $body .= "Message : $message\n";
-        $headers = "From: enzolemercier@gmail.com";
-        mail($to, $subject, $body, $headers);
-
+        $_SESSION['prenom_user'] = htmlspecialchars($prenom);
         header('location:validation.php');
     } else {
         echo "<h1>Une erreur est survenue</h1>";
